54d2d02149f02273238bbc7dc1c2307afc030569
Manuel Sánchez <manuelf.sanchez@smartmatic.com>
2022-06-17 23:32:57 -0500

    Add components functionality

---
+++ src/components/HeaderMenu.vue
@@ -0,0 +1,40 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">{{ brand }} - {{ title }}</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            
+        </div>
+    </nav> 
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Insertar cama</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="#">Actualizar cama</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="#">Consultar cama</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        props:[
+            'brand',
+            'title'
+        ],
+        data(){
+            return {
+                
+            };
+        }        
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
-import { createApp } from 'vue'
+import "bootstrap/dist/css/bootstrap.css";
+import { createApp } from 'vue';
+import App from './App.vue';
+import HeaderMenu from './components/HeaderMenu.vue';
+import InsertUpdatePatient from './components/InsertUpdatePatient.vue';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.component('insert-update-patient', InsertUpdatePatient);
+app.component('header-menu', HeaderMenu);
+
+app.mount('#app');
+
+import "bootstrap/dist/js/bootstrap.js"
